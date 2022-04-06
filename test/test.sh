ef297689d0c80530ce2dd3beda8293ea68b5b7a1
Nikhil Kumar <nikhilkumar516@gmail.com>
2022-04-06 14:27:40 -0400

    Updated tmp directory path

--- test/test.sh
+++ test/test.sh
@@ -6,7 +6,8 @@ PROCESS_LIST=()
 run_test() {
 	mkdir -p ${1}/${2}/work
 	mkdir -p ${1}/${2}/out
-	toil-cwl-runner --singularity --logFile ${1}/${2}/toil_log.log --coalesceStatusCalls --debug --clean never --disableCaching --batchSystem lsf --doubleMem  --disable-user-provenance --disable-host-provenance --preserve-environment PATH TMPDIR TOIL_LSF_ARGS SINGULARITY_PULLDIR SINGULARITY_CACHEDIR PWD SINGULARITY_DOCKER_USERNAME SINGULARITY_DOCKER_PASSWORD SINGULARITYENV_LC_ALL --defaultMemory 8G --maxCores 16 --maxDisk 128G --maxMemory 256G --not-strict --realTimeLogging  --jobStore ${1}/${2}/jobstore --tmpdir-prefix /scratch --workDir ${1}/${2}/work --outdir ${1}/${2}/out --maxLocalJobs 500 ${3} ${4} &
+	mkdir -p ${1}/${2}/tmp
+	toil-cwl-runner --singularity --logFile ${1}/${2}/toil_log.log --coalesceStatusCalls --debug --clean never --disableCaching --batchSystem lsf --doubleMem  --disable-user-provenance --disable-host-provenance --preserve-environment PATH TMPDIR TOIL_LSF_ARGS SINGULARITY_PULLDIR SINGULARITY_CACHEDIR PWD SINGULARITY_DOCKER_USERNAME SINGULARITY_DOCKER_PASSWORD SINGULARITYENV_LC_ALL --defaultMemory 8G --maxCores 16 --maxDisk 128G --maxMemory 256G --not-strict --realTimeLogging  --jobStore ${1}/${2}/jobstore --tmpdir-prefix ${1}/${2}/tmp --workDir ${1}/${2}/work --outdir ${1}/${2}/out --maxLocalJobs 500 ${3} ${4} &
 	PROCESS_LIST+=($!)
 }
 
