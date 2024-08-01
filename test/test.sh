74c2ce060b597cfddf87f4ca9b75c5cae2af237e
D-Pankey <30415217+D-Pankey@users.noreply.github.com>
2024-08-01 13:43:07 -0400

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -7,7 +7,7 @@ run_test() {
 	mkdir -p ${1}/${2}/work
 	mkdir -p ${1}/${2}/out
 	mkdir -p ${1}/${2}/tmp
-	toil-cwl-runner --singularity --logFile ${1}/${2}/toil_log.log --coalesceStatusCalls --debug --clean --disableCaching --batchSystem lsf --doubleMem  --disable-user-provenance --disable-host-provenance --preserve-environment PATH TMPDIR TOIL_LSF_ARGS SINGULARITY_PULLDIR SINGULARITY_CACHEDIR PWD SINGULARITY_DOCKER_USERNAME SINGULARITY_DOCKER_PASSWORD SINGULARITYENV_LC_ALL --defaultMemory 8G --maxCores 16 --maxDisk 128G --maxMemory 256G --not-strict --realTimeLogging  --jobStore ${1}/${2}/jobstore --tmpdir-prefix ${1}/${2}/tmp --workDir ${1}/${2}/work --outdir ${1}/${2}/out --maxLocalJobs 500 ${3} ${4} &
+	toil-cwl-runner --singularity --logFile ${1}/${2}/toil_log.log --coalesceStatusCalls --debug --clean always --disableCaching --batchSystem lsf --doubleMem  --disable-user-provenance --disable-host-provenance --preserve-environment PATH TMPDIR TOIL_LSF_ARGS SINGULARITY_PULLDIR SINGULARITY_CACHEDIR PWD SINGULARITY_DOCKER_USERNAME SINGULARITY_DOCKER_PASSWORD SINGULARITYENV_LC_ALL --defaultMemory 8G --maxCores 16 --maxDisk 128G --maxMemory 256G --not-strict --realTimeLogging  --jobStore ${1}/${2}/jobstore --tmpdir-prefix ${1}/${2}/tmp --workDir ${1}/${2}/work --outdir ${1}/${2}/out --maxLocalJobs 500 ${3} ${4} &
 	PROCESS_LIST+=($!)
 }
 
