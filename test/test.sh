7f17db36fa4f311c79edaeecb46e4085e144e6fb
D-Pankey <30415217+D-Pankey@users.noreply.github.com>
2024-08-06 17:03:21 -0400

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -13,19 +13,19 @@ run_test() {
 
 # Test the SV cwl
 
-#run_test $1 sv ../project-workflow-sv.cwl inputs.yaml
+run_test $1 sv ../project-workflow-sv.cwl inputs.yaml
 
 # Test the non SV cwl
 
- run_test $1 non_sv ../project-workflow.cwl inputs.yaml
+# run_test $1 non_sv ../project-workflow.cwl inputs.yaml
 
 # Test the copy outputs cwl
 
-run_test $1 copy_outputs ../workflows/copy_outputs.cwl copy_outputs.yaml
+#run_test $1 copy_outputs ../workflows/copy_outputs.cwl copy_outputs.yaml
 
 # Test the copy outputs cwl without meta
 
-run_test $1 copy_outputs_without_meta ../workflows/copy_outputs.cwl copy_outputs_without_meta.yaml
+#run_test $1 copy_outputs_without_meta ../workflows/copy_outputs.cwl copy_outputs_without_meta.yaml
 
 for SINGLE_PROCESS in "${PROCESS_LIST[@]}"; do
   wait "$SINGLE_PROCESS"
